930a01a094e6a55b0f07ca2cbd7beca399575d30
KzDx <deftsing21@hotmail.com>
2023-02-28 19:21:31 +0100

    carga y análisis de datos

--- KzDx/app.py
+++ KzDx/app.py
@@ -6,6 +6,6 @@ def load_dataset(data_link):
     dataset = pd.read_csv(data_link)
     return dataset
 
-titanic_link = "https://raw.githubusercontent.com/fivethirtyeight/data/master/covid-geography/mmsa-icu-beds.csv"
-titanic_data = load_dataset(titanic_link)
-st.dataframe(titanic_data)
+database_link = "https://raw.githubusercontent.com/fivethirtyeight/data/master/covid-geography/mmsa-icu-beds.csv"
+database_data = load_dataset(database_link)
+st.dataframe(database_data)
